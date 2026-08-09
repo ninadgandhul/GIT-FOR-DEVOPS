dfd0d93430b3588cc6ad65a2914f2517888aa244
ninadgandhul <ninadgandhul2@gmail.com>
2026-08-09 15:01:59 +0530

    updated code

--- testing.py
+++ testing.py
@@ -1,3 +1,8 @@
+
+print("ninad")
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+print(f"The sum is {num1 + num2}")   
 print("ninad")
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
